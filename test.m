b49b1f75132251b9e5ae0ec3abf8131e01a34213
Yi Zhang <zhang9@outlook.de>
2021-09-10 12:44:54 +0200

    add test

--- test.m
+++ test.m
@@ -1,3 +1,15 @@
-if 1==0
-    error('error is too big');
+e=zeros(3,1);
+
+calculated=3;
+reference=2;
+threshold=0.5;
+e(1)=abs(calculated-reference)>threshold; % error1 occured
+e(2)=0; % error2 not occured
+e(3)=0; % error3 occured
+
+fprintf('test result \n');
+disp(e)
+
+if sum(e)>=1
+    error('error occured in the test');
 end
