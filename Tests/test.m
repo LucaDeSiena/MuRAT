de16e5a3b91e5d862b6f9d707cfd62e2928c27b0
Yi Zhang <zhang9@outlook.de>
2021-09-10 13:11:39 +0200

    add test

--- Tests/test.m
+++ Tests/test.m
@@ -7,7 +7,11 @@ threshold=1;
 e(1)=abs(calculated-reference)>threshold; % error1 occured
 e(2)=0; % error2 not occured
 e(3)=0; % error3 not occured
-
+%% test if parallelization can be used
+parfor i=1:3
+    a=i;
+end
+%%
 fprintf('error_status \n');
 disp(e);
 
