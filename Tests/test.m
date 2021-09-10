28739ecc9abbdda05734c569ea2554313ef1cad3
Yi Zhang <zhang9@outlook.de>
2021-09-10 12:54:46 +0200

    add test

--- Tests/test.m
+++ Tests/test.m
@@ -1,16 +1,17 @@
 e=zeros(3,1);
 
 calculated=3;
-reference=2;
-threshold=0.5;
+reference=2.5;
+threshold=1;
+
 e(1)=abs(calculated-reference)>threshold; % error1 occured
 e(2)=0; % error2 not occured
-e(3)=0; % error3 occured
-e(1)=0;
+e(3)=0; % error3 not occured
 
-fprintf('test result \n');
-disp(e)
+fprintf('error_status \n');
+disp(e);
 
 if sum(e)>=1
+
     error('error occured in the test');
 end
