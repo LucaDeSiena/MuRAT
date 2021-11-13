41234597ad20c080c86dd13d4a97cb6c7156e4f1
Luca De Siena <lucadesiena80@gmail.com>
2021-11-13 09:05:00 +0100

    Solved bug mpd
    When you divide by the sum on parameter models with no ray you get a NaN that shows up in the map: good or bad?
    
    Co-Authored-By: mreissuf <93253333+mreissuf@users.noreply.github.com>

--- bin/Murat_inversion.m
+++ bin/Murat_inversion.m
@@ -98,6 +98,8 @@ for k = 1:lMF(2)
     
     mpd                             =...
         sum(Apd_k.*lpdelta_k,1)'./sum(Apd_k,1)';
+    mpd(isnan(mpd))                 =   0;
+    
     modv_pd(rcpd_k,4,k)             =   mpd;
     
     %%
