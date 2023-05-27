f22159f243d10a27eb3e888cb90b7cabf091ba01
Luca De Siena <lucadesiena80@gmail.com>
2023-05-27 18:33:40 +0200

    removing IRTOOLS folder

--- IRtools/Extra/prblur_tools/@kronMatrix/kron.m
+++
@@ -1,9 +0,0 @@
-function m = kron(k)
-
-%  kronMatrix kron(k)
-%
-%     returns the matrix obtained by expanding the matrix k
-%     using the Kronecker product
-%
-
-m = kron(k.a,k.b);
--- IRtools/Extra/prblur_tools/@kronMatrix/max.m
+++
@@ -1,6 +0,0 @@
-function x = max(k)
-%  kronMatrix max(k)
-%
-%
-
-x = kronMatrix(max(k.a), max(k.b));
